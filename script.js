d06e3a9ee6dcac6e35c032721fad2eef0f6634fd
Isailton89 <113316233+Isailton89@users.noreply.github.com>
2023-07-19 22:53:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const card = document.getElementById('box-card')
+
+chatsjson()
+async function chatsjson() {
+	const conexao = await fetch('https://raw.githubusercontent.com/OperacaoN1/Escala/main/chatbot.json')
+	const conexaoConvertida = await conexao.json()
+	conexaoConvertida.forEach((item) => {
+		const servico = item.servico
+		const link = item.link
+		const novaDiv = document.createElement('div')
+		novaDiv.classList.add('card')
+
+		novaDiv.innerHTML = `
+			<img src="/img/robo.svg" alt="icone de um robô">
+			<a href=${link}>${servico}</a>
+		`
+
+		card.appendChild(novaDiv)
+	})
+}
