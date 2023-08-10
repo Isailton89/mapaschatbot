bd2f793ad6118cbaa1f6f90e02119441ef2a6ec2
Isailton89 <113316233+Isailton89@users.noreply.github.com>
2023-08-10 08:15:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-const card = document.getElementById('box-card')
+const boxCard = document.getElementById('box-card')
+const boxVersoes = document.getElementById('versoes')
 
 chatsjson()
 async function chatsjson() {
 	const conexao = await fetch('https://raw.githubusercontent.com/OperacaoN1/Escala/main/chatbot.json')
 	const conexaoConvertida = await conexao.json()
 	conexaoConvertida.forEach((item) => {
-		const servico = item.servico
-		const link = item.link
-		const novaDiv = document.createElement('div')
-		novaDiv.classList.add('card')
-		novaDiv.id= "clickDiv"
+		const card = document.createElement('div')
+		const nomeServico = item.servico
+		const texto = document.createElement('span')
+		texto.textContent = nomeServico 
+		card.classList.add('card')
+		card.appendChild(texto)
+		boxCard.appendChild(card)
 
-		novaDiv.innerHTML = `
-			<img src="/img/robo.svg" alt="icone de um robô">
-			<a href=${link}>${servico}</a>
-		`
-
-		card.appendChild(novaDiv)
-
-		novaDiv.addEventListener("click", function() {
-      		let linkA = this.querySelector("a");
-      		if (linkA) {
-        		window.location.href = linkA.href;
-      		}
-    	})
+		card.addEventListener("click", () => {
+			boxVersoes.innerHTML = ''
+			item.versao.forEach(version => {
+				const cardVersao = document.createElement('div')
+				const textoVersao = document.createElement('p')
+				const nomeVersao = document.createElement('a')
+				const classeVersao = version.estado
+				textoVersao.textContent = `Versões disponiveis do BOT ${item.servico.toUpperCase()}`
+				boxVersoes.appendChild(textoVersao)
+				cardVersao.classList.add(classeVersao)
+				nomeVersao.textContent = version.nome
+				nomeVersao.setAttribute("href", version.link)
+				cardVersao.appendChild(nomeVersao)
+				boxVersoes.appendChild(cardVersao)
+				
+			})
+		})
 	})
 }
 
